ba085abe288fad321c5262227dd2fc7d46552c6b
Busyra <busyraabdullah0312@gmail.com>
2024-05-06 03:20:51 +0800

    login signup

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 import bcrypt
 
@@ -9,26 +9,16 @@ auth = Blueprint('auth', __name__)
 
 def hash_password(password):
     salt = bcrypt.gensalt()
-    password = request.form.get('password')
-    hashed_password = bcrypt.hashpw(password, salt)
-    return hashed_password
-    
-def get_hashed_password(password):
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password, salt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
-def get_hashed_password(username):
+def get_hashed_password(password):
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(username, salt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    hashed_password = get_hashed_password('password')
-    if hashed_password is None:
-        return None
-    
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -46,19 +36,14 @@ def login():
 
     return render_template("login.html", user=current_user)
 
-
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    salt = bcrypt.gensalt()
-    password = b'my_password'
-    hashed_password = hash_password(password)
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
@@ -77,8 +62,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=hash_password(password1))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
