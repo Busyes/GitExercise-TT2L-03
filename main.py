7a88774f9e7b63984865f01b415ef7d69860c1ca
Busyra <busyraabdullah0312@gmail.com>
2024-05-10 18:00:57 +0800

    fixed link pages

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from website import create_app
 from flask import render_template, request, redirect, session
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+import bcrypt
 
-app = create_app()
+main = create_app()
+
+index = Blueprint('index', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/sound')
+def sound():
+    return render_template('sound.html')
+
+@main.route('/alarm')
+def alarm():
+    return render_template('alarm.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main.run(debug=True)
 
