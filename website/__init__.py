da9be534077ed88a749fec21e222b7d52e6527b5
nurulasyiq <nnurulsiqin@gmail.com>
2024-05-13 06:27:42 +0800

    fix p2

--- website/__init__.py
+++ website/__init__.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
 DB_NAME = "database.db"
 
 
@@ -13,9 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-
     db.init_app(app)
-    bcrypt.init_app(app)
 
     from .views import views
     from .auth import auth
