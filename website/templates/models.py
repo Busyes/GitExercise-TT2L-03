5cf6fc2b98d5027c29afe8b3f10115d9d08ed235
Amira Raheema <amiraraheema05@gmail.com>
2024-05-25 02:26:13 +0800

    tracker

--- website/templates/models.py
+++ website/templates/models.py
@@ -1,6 +1,9 @@
 from .. import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 #supposedly timer goes here
 class Note(db.Model):
@@ -10,10 +13,15 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 #table for user info
-#retry
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    sessions = db.relationship('UserSession')
+
+class UserSession(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    num_session = db.Column(db.Integer, default=0)
