53dd262ebbcf070bcdec9353835f15ce23b56c22
Busyra <busyraabdullah0312@gmail.com>
2024-05-27 16:00:17 +0800

    messages in sound page

---
+++ website/static/script.js
@@ -0,0 +1,19 @@
+function playMessage() {
+	document.getElementById("message").innerText = "Background song 1 is a default sound for each session, other background song will unlocked once user reach targeted session!";
+}
+
+function playMessage2() {
+	document.getElementById("message 2").innerText = "This song will unlocked once you have reached 3 sessions";
+}
+
+function playMessage3() {
+	document.getElementById("message 3").innerText = "This song will unlocked once you have reached 5 sessions";
+}
+
+function playMessage4() {
+	document.getElementById("message 4").innerText = "This song will unlocked once you have reached 7 sessions";
+}
+
+function playMessage5() {
+	document.getElementById("message 5").innerText = "This song will unlocked once you have reached 9 sessions";
+}
