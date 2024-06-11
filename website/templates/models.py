1690a6bc67c987724bbffbb7fc3cec9e239b1cf2
AmiraRatatouliee <167070997+AmiraRatatouliee@users.noreply.github.com>
2024-06-12 01:24:35 +0800

    tracker weekly and monthly

--- website/templates/models.py
+++ website/templates/models.py
@@ -1,25 +1,24 @@
-from .. import db
+from flask import Flask
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import os
+
+from .. import db
 
 app = Flask(__name__)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-#supposedly timer goes here
+# supposedly timer goes here
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-#table for user info
+
+# table for user info
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
@@ -28,8 +27,15 @@ class User(db.Model, UserMixin):
     notes = db.relationship('Note')
     sessions = db.relationship('UserSession')
 
+
 class UserSession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    num_session = db.Column(db.Integer, default=0)
     category = db.Column(db.String(150))
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+# class UserSession2(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     num_session = db.Column(db.Integer, default=0)
+#     category = db.Column(db.String(150))
